2f90b468b52b6ee46f0488c64a2252bdad93f675
Jeffrey Kritzman <24811122+jeffkritzman@users.noreply.github.com>
2021-11-16 09:43:57 -0600

    Add files via upload

--- GetUSMDataBySvc.sql
+++ GetUSMDataBySvc.sql
@@ -13,16 +13,16 @@ AS
 
 	SET NOCOUNT ON;
 
-	DROP TABLE IF EXISTS #weeklySvc
+	--get hourly summed census for each service
+	DROP TABLE IF EXISTS #hourlySvc
 	SELECT
-		usm.ServiceCode, MAX(hourly.census) AS max_weekly_census
-		, dt.DATE_WEEK_BEGIN_DATE
-	INTO #weeklySvc
-	FROM adt.usm usm
-	JOIN FinBI.adt.UnitServiceMapCoreByHour hourly
+		usm.ServiceCode, hourly.enddttm
+		, SUM(hourly.census) AS summed_hourly_census
+	INTO #hourlySvc
+	FROM adt.usm usm --granularity = dept + svc, has high level stats
+	JOIN FinBI.adt.UnitServiceMapCoreByHour hourly --granularity = hour + dept + svc, good for seeing granular system state
 		ON hourly.serviceCode = usm.ServiceCode
-	JOIN HEDIReporting..DIM_DATE dt
-		ON dt.DATE_CALENDAR_DATE = DATEFROMPARTS(YEAR(enddttm), MONTH(enddttm), DAY(enddttm))
+		AND hourly.DEPARTMENT_ID = usm.DEPARTMENT_ID
 	WHERE 1=1 
 		AND usm.DEPARTMENT_ID IS NOT null 
 		AND usm.ServiceCode IS NOT NULL
@@ -30,11 +30,26 @@ AS
 		AND usm.CareAreaSpecialty IS NOT NULL
 		AND usm.AvgDailyCensus IS NOT NULL
 		AND usm.AvgDailyCensus > 0
-	GROUP BY usm.ServiceCode, dt.DATE_WEEK_BEGIN_DATE
-	ORDER BY usm.ServiceCode, dt.DATE_WEEK_BEGIN_DATE
+	GROUP BY usm.ServiceCode, hourly.enddttm
+	ORDER BY usm.ServiceCode, hourly.enddttm
 
+	--for each service, find the maximum hourly census for each week
+	DROP TABLE IF EXISTS #weeklySvc
+	SELECT
+		hourly.ServiceCode, dt.DATE_WEEK_BEGIN_DATE
+		, MAX(hourly.summed_hourly_census) AS max_weekly_census
+	INTO #weeklySvc
+	FROM #hourlySvc hourly --granularity = hour + service
+	JOIN HEDIReporting..DIM_DATE dt 
+		ON dt.DATE_CALENDAR_DATE = DATEFROMPARTS(YEAR(hourly.enddttm), MONTH(hourly.enddttm), DAY(hourly.enddttm))
+	WHERE 1=1 
+	GROUP BY hourly.ServiceCode, dt.DATE_WEEK_BEGIN_DATE
+	ORDER BY hourly.ServiceCode, dt.DATE_WEEK_BEGIN_DATE
+
+	--for each service, take the average of the weekly maximum hourly census - use this as the patient demand to be a little conservative, but not overly so
 	SELECT main.ServiceCode, AVG(main.max_weekly_census) AS demand
-	FROM #weeklySvc main
+	FROM #weeklySvc main --granularity = week + service
 	GROUP BY main.ServiceCode
+	ORDER BY 2, main.ServiceCode
 
 
